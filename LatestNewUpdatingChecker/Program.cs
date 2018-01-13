da2f07dec901076111f0d926a600488d8e8efd06
Iskander_Ismagilov <isma.translator@gmail.com>
2018-01-13 21:59:51 +0500

    Corrected main variables names of the program.

--- LatestNewUpdatingChecker/Program.cs
+++ LatestNewUpdatingChecker/Program.cs
@@ -1,22 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace LatestNewUpdatingChecker
 {
     static class Program
     {
+        private const string dataFileName = "Data.txt";
+        private const string _newsPageContentFileName = "NewsPageContent.txt";
+
+        private static string thisProgramPath => Assembly.GetEntryAssembly().Location;        
+        static string currentDirectory = Path.GetDirectoryName(thisProgramPath);
+        static string dataFilePath = Path.Combine(currentDirectory, dataFileName);
+
+        //SetStartUp(true);
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Checker checker = new Checker();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Form mainForm = new Form1(checker);
+            Application.Run(mainForm);
+
+            while (true)
+            {
+                if (CheckForNewNews(dataFilePath))
+                {
+                    Console.WriteLine("На сайте есть новая новость.");
+                    Process.Start("http://frgsrb.ru/news/?SECTION_ID=485");
+                }
+                else
+                {
+                    TimeSpan forOneHour = new TimeSpan(1, 0, 0);
+                    Thread.Sleep(forOneHour);
+                }
+            }
         }
+
+        //private static void SetStartUp(bool set)
+        //{
+        //    RegistryKey rk = Registry.CurrentUser.OpenSubKey
+        //    ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        //    if (set)
+        //    {
+        //        if ((rk.GetValue("TheApartmentsSellingNews") == null))
+        //        {
+        //            rk.SetValue("TheApartmentsSellingNews", thisProgramLocation);
+        //        }
+        //    }
+        //    else
+        //        rk.DeleteValue("TheApartmentsSellingNews", false);
+        //}
     }
 }
