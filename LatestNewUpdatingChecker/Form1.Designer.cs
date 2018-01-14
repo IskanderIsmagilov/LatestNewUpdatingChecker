231b0299df710a06ef23439384a7f12b1ba1c7fa
Iskander_Ismagilov <isma.translator@gmail.com>
2018-01-14 14:05:28 +0500

    Two buttons in FormDesigner replaced by one checkbox.

--- LatestNewUpdatingChecker/Form1.Designer.cs
+++ LatestNewUpdatingChecker/Form1.Designer.cs
@@ -38,11 +38,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBoxEMail = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBoxStartWithWindows = new System.Windows.Forms.CheckBox();
             this.textBoxNotes = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.checkBoxIsChecking = new System.Windows.Forms.CheckBox();
             toolTipHtml = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
@@ -66,7 +65,7 @@
             this.textBoxNewsPage.Name = "textBoxNewsPage";
             this.textBoxNewsPage.Size = new System.Drawing.Size(440, 22);
             this.textBoxNewsPage.TabIndex = 1;
-            this.textBoxNewsPage.TextChanged += new System.EventHandler(this.WebPage_TextChanged);
+            this.textBoxNewsPage.TextChanged += new System.EventHandler(this.textBoxNewsPage_TextChanged);
             // 
             // textBoxHtml
             // 
@@ -121,42 +120,24 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "E-mail:";
             // 
-            // button1
+            // checkBoxStartWithWindows
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(28, 180);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(334, 158);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Start checking";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(389, 180);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(334, 158);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Stop checking";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.checkBox1.Location = new System.Drawing.Point(659, 24);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(234, 33);
-            this.checkBox1.TabIndex = 10;
-            this.checkBox1.Text = "Start with windows";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBoxStartWithWindows.AutoSize = true;
+            this.checkBoxStartWithWindows.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxStartWithWindows.Location = new System.Drawing.Point(659, 24);
+            this.checkBoxStartWithWindows.Name = "checkBoxStartWithWindows";
+            this.checkBoxStartWithWindows.Size = new System.Drawing.Size(234, 33);
+            this.checkBoxStartWithWindows.TabIndex = 10;
+            this.checkBoxStartWithWindows.Text = "Start with windows";
+            this.checkBoxStartWithWindows.UseVisualStyleBackColor = true;
+            this.checkBoxStartWithWindows.CheckedChanged += new System.EventHandler(this.checkBoxStartWithWindows_CheckedChanged);
             // 
             // textBoxNotes
             // 
             this.textBoxNotes.Location = new System.Drawing.Point(197, 141);
+            this.textBoxNotes.Multiline = true;
             this.textBoxNotes.Name = "textBoxNotes";
-            this.textBoxNotes.Size = new System.Drawing.Size(440, 22);
+            this.textBoxNotes.Size = new System.Drawing.Size(440, 183);
             this.textBoxNotes.TabIndex = 12;
             // 
             // label5
@@ -169,16 +150,27 @@
             this.label5.TabIndex = 11;
             this.label5.Text = "Notification:";
             // 
+            // checkBoxIsChecking
+            // 
+            this.checkBoxIsChecking.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBoxIsChecking.AutoSize = true;
+            this.checkBoxIsChecking.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxIsChecking.Location = new System.Drawing.Point(813, 285);
+            this.checkBoxIsChecking.Name = "checkBoxIsChecking";
+            this.checkBoxIsChecking.Size = new System.Drawing.Size(174, 39);
+            this.checkBoxIsChecking.TabIndex = 13;
+            this.checkBoxIsChecking.Text = "Start checking";
+            this.checkBoxIsChecking.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1012, 350);
+            this.Controls.Add(this.checkBoxIsChecking);
             this.Controls.Add(this.textBoxNotes);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBoxStartWithWindows);
             this.Controls.Add(this.textBoxEMail);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBoxLastId);
@@ -205,11 +197,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBoxEMail;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBoxStartWithWindows;
         private System.Windows.Forms.TextBox textBoxNotes;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBoxIsChecking;
     }
 }
 
