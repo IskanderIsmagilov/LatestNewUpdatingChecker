8bd6e9724a0b47dc9693696d2aa4376afdaaed0e
Iskander_Ismagilov <isma.translator@gmail.com>
2018-01-14 13:50:43 +0500

    Added Starter class .

---
+++ LatestNewUpdatingChecker/Starter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+
+namespace LatestNewUpdatingChecker
+{
+    static class Starter
+    {
+        private const string _thisProgramName = "LastNewsUpdatingChecker";
+        private const string _runSubKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+        public static string ThisProgramPath
+        {
+            get { return ThisProgramPath; }
+            set
+            {
+                if (string.IsNullOrEmpty(ThisProgramPath)) ThisProgramPath = value;
+            }
+        }       
+
+        public static void SetStartUp(bool set)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey(_runSubKey, true);
+
+            if (set)
+            {
+                if ((rk.GetValue(_thisProgramName) == null))
+                {
+                    rk.SetValue(_thisProgramName, ThisProgramPath);
+                }
+            }
+            else
+                rk.DeleteValue(_thisProgramName, false);
+        }
+    }
+}
